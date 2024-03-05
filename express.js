302fef3e52c156930f4f65ab0ca64f35e34386db
yibeltal-gashaw <yibeltalgashaw320@gmail.com>
2024-03-05 16:45:37 +0300

    add file

--- express.js
+++ express.js
@@ -1,12 +1,9 @@
-if (process.env.NODE_ENV!== 'production') {
-  require('dotenv').config()
-}
-
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const  authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -14,13 +11,15 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static(__dirname + '/public'))
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function () {
-  console.log('Connected to MongoDB')
-})
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+// const db = mongoose.connection
+// db.on('error', console.error.bind(console, 'connection error:'))
+// db.once('open', function () {
+//   console.log('Connected to MongoDB')
+// })
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 app.listen(process.env.PORT || 3000)
